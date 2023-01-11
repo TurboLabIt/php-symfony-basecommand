0ba2496f7f5c91528c882b5e32bc3ccff612de82
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-11 21:36:07 +0100

    Update CliArgumentsTrait.php

--- src/Traits/CliArgumentsTrait.php
+++ src/Traits/CliArgumentsTrait.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Input\InputArgument;
 
 trait CliArgumentsTrait
 {
-    protected function getCliOption(string $staticCLI_ARG_NAME) : mixed
+    protected function getCliArgument(string $staticCLI_ARG_NAME) : mixed
     {
         return $this->input->getArgument($staticCLI_ARG_NAME);
     }

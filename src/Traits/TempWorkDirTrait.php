6233fbcbe75f3410cf9837f977ed4276bf22273c
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-12 08:44:03 +0100

    Create TempWorkDirTrait.php

---
+++ src/Traits/TempWorkDirTrait.php
@@ -0,0 +1,22 @@
+<?php
+namespace TurboLabIt\PhpSymfonyBasecommand\Traits;
+
+
+trait TempWorkDirTrait
+{
+    protected function getTempWorkingDirPath() : string
+    {
+        $path = sys_get_temp_dir();
+        
+        // make sure it has a trailing slash
+        $path .= substr($path, -1) == DIRECTORY_SEPARATOR ? '' : DIRECTORY_SEPARATOR;
+      
+        $path .= $this->getName() . DIRECTORY_SEPARATOR;
+        
+        if( !is_dir($path) ) {
+            mkdir($path);
+        }
+        
+        return $path;
+    }
+}

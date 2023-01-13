cd218127e8abbd4eaab0de4496645045df2900d2
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-13 08:58:05 +0100

    Create CsvHandlerTrait.php

---
+++ src/Traits/CsvHandlerTrait.php
@@ -0,0 +1,30 @@
+<?php
+namespace TurboLabIt\PhpSymfonyBasecommand\Traits;
+
+
+trait CsvHandlerTrait
+{
+    int $lastCsvReadRowsNum = 0;
+    
+    
+    protected function readCsv(string $csvFilePath, bool $silent = off, string $delimiter = ',', int $headerOffset = 0) : \League\Csv\MapIterator
+    {
+        if( !$silent ) {
+            $this->fxInfo("📑 Accessing CSV ##" . $csvFilePath . "##");
+        }
+        
+        $csvFile = \League\Csv\Reader\Reader::createFromPath($csvFilePath);
+        $csvFile->setDelimiter($delimiter);
+        $csvFile->setHeaderOffset($headerOffset);
+        
+        $this->lastCsvReadRowsNum = count($csvFile);
+        
+        $oCsvData = $csvFile->getRecords();
+        
+        if( !$silent ) {
+            $this->fxOK("🔢 CSV ready. ##" . number_format($this->lastCsvReadRowsNum, 0, ',', '.') . "## row(s) returned");
+        }
+        
+        return $oCsvData;
+    }
+}

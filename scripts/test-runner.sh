a570e7ab9849d1f37d1991548f13338706e3641d
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2023-01-24 11:59:40 +0100

    Update composer and test

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -1,39 +1,10 @@
 #!/usr/bin/env bash
+## Test this package!
 
-## https://github.com/TurboLabIt/webstackup/blob/master/script/base.sh
-source "/usr/local/turbolab.it/webstackup/script/base.sh"
-fxHeader "🧪 BaseCommand Tester"
-EXPECTED_USER=$(logname)
+APP_NAME=BaseCommand
+source "/usr/local/turbolab.it/webstackup/script/php/test-runner-package.sh"
 
-cd $PROJECT_DIR
-
-wsuComposer install
-
-phpunit \
-  --bootstrap vendor/autoload.php \
-  --cache-result-file=/tmp/.phpunit.result.cache \
-  --stop-on-failure \
-  tests
-
-PHPUNIT_RESULT=$?
-echo ""
-
-if [ "${PHPUNIT_RESULT}" = 0 ]; then
-
-  fxMessage "🎉 TEST RAN SUCCESSFULLY!"
-  fxCountdown 3
-
-  fxTitle "Running the test command..."
-  php tests/RunTestCommand.php
-  wsuPlayOKSound
-
-else
-
-  fxMessage "🛑 TEST FAILED | phpunit returned ${PHPUNIT_RESULT}"
-  wsuPlayKOSound
-fi
-
-fxTitle "Cleaning up..."
+fxTitle "🧹 Cleaning up..."
 rm -rf /tmp/BaseCommandTestInstance
 
 fxEndFooter

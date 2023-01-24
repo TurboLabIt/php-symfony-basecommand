57b015f363fc1434d37eb8adb0933fa232b263ed
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2023-01-24 15:22:14 +0100

    Update composer and test

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
-## Test this package!
+## Run the project tests and check the result
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/test-runner.sh
 
-APP_NAME=BaseCommand
+TESTR_SCRIPT_BEGIN=$(dirname $(readlink -f $0))/script_begin.sh
+if [ -f "${TESTR_SCRIPT_BEGIN}" ]; then
+  source "${TESTR_SCRIPT_BEGIN}"
+else
+  source "/usr/local/turbolab.it/webstackup/script/base.sh"
+  APP_NAME=BaseCommand
+  EXPECTED_USER=$(logname)
+fi
+
+
+fxHeader "🧪 ${APP_NAME} Test Runner"
+
+# https://github.com/TurboLabIt/webstackup/tree/master/script/php/test-runner-package.sh
 source "/usr/local/turbolab.it/webstackup/script/php/test-runner-package.sh"
 
 fxTitle "🧹 Cleaning up..."
 rm -rf /tmp/BaseCommandTestInstance
 
-fxEndFooter
+TESTR_SCRIPT_END=$(dirname $(readlink -f $0))/script_end.sh
+if [ -f "${TESTR_SCRIPT_END}" ]; then
+  source "${TESTR_SCRIPT_END}"
+else
+  fxEndFooter
+fi

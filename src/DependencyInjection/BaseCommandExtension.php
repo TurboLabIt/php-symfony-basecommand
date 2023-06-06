61c75d4830e9d5114af9a79a93f4447c74db06d5
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2023-06-06 16:07:42 +0200

    symfony bundle

---
+++ src/DependencyInjection/BaseCommandExtension.php
@@ -0,0 +1,17 @@
+<?php
+namespace TurboLabIt\BaseCommand\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+class BaseCommandExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/config'));
+        $loader->load('services.yaml');
+    }
+}

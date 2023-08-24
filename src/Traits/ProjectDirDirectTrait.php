79d1dec866b12f1972364b6fe593edbe7ce18594
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-08-24 10:25:06 +0200

    Update ProjectDirDirectTrait.php

--- src/Traits/ProjectDirDirectTrait.php
+++ src/Traits/ProjectDirDirectTrait.php
@@ -6,9 +6,6 @@ use TurboLabIt\BaseCommand\Service\ProjectDir;
 
 trait ProjectDirDirectTrait
 {
-    protected ?ProjectDir $projectDir;
-
-
     public function getProjectDir(array|string $subpath = '') : string
     {
         return $this->projectDir->getProjectDir($subpath);

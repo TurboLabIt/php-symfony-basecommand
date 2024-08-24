bba9d042e387f53411d8e26449fde824ad187041
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-08-24 08:51:39 +0200

    Update EnvTrait.php

--- src/Traits/EnvTrait.php
+++ src/Traits/EnvTrait.php
@@ -2,6 +2,7 @@
 namespace TurboLabIt\BaseCommand\Traits;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use RuntimeException;
 
 
 trait EnvTrait
@@ -9,20 +10,20 @@ trait EnvTrait
     protected ParameterBagInterface $parameters;
 
 
-    public function isProd() : bool
+    public function getEnv() : string
     {
-        return $this->parameters->get("kernel.environment") === "prod";
+        if( empty($this->parameters) ) {
+            throw new RuntimeException(
+                'Autowiring ParameterBagInterface $parameters is required before using BaseCommand env functions'
+            );
+        }
+
+        return $this->parameters->get("kernel.environment")
     }
 
 
-    public function isNotProd() : bool
-    {
-        return !$this->isProd();
-    }
-
-
-    public function isDev() : bool
-    {
-        return $this->parameters->get("kernel.environment") === "dev";
-    }
+    public function isProd() : bool { return $this->getEnv() === "prod"; }
+    public function isNotProd() : bool { return !$this->isProd(); }
+    public function isDev() : bool { return $this->getEnv() === "dev"; }
+    public function isDevOrTest() : bool { return in_array($this->getEnv(), ["dev", "test"]); }
 }

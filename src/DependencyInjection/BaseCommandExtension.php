f8a1ecbd8b2eb4a0ef0111d4008d4693e6510901
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-12-26 19:22:38 +0100

    deprecation fix

--- src/DependencyInjection/BaseCommandExtension.php
+++ src/DependencyInjection/BaseCommandExtension.php
@@ -1,7 +1,7 @@
 <?php
 namespace TurboLabIt\BaseCommand\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
